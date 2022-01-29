f86eae66f3f9fd2ef700a6a6658f5a18efdab016
Alimov Tillo <alimovtillo@gmail.com>
2022-01-29 22:39:33 +0600

    Added models to Services

--- project/apps/services/admin.py
+++ project/apps/services/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Service
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        obj.save()
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['title', 'published', 'category']
+    list_filter = ['category']
+
+    search_fields = ['title', 'description']
+    prepopulated_fields = {"slug": ("title",)}
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        obj.save()
+
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Service, ServiceAdmin)
