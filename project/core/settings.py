c17da929715394a1c44cf32a32e54918df383391
Alimov Tillo <alimovtillo@gmail.com>
2022-03-15 14:30:37 +0600

    f

--- project/core/settings.py
+++ project/core/settings.py
@@ -145,14 +145,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+STATICFILES_DIRS = [BASE_DIR / 'assets']
+
 STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_DIRS = (
-    'assets',
-)
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DOMAIN = env('DOMAIN')
